c16a8bf638e8313b84457a625df54be3945101cb
peter2276 <pedrodelacruz18@hotmail.com>
2022-08-27 12:17:22 -0300

    Commit inicial. Primera configuracion del mcc

---
+++ main.c
@@ -0,0 +1,71 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.8
+        Device            :  PIC18F25K50
+        Driver Version    :  2.00
+*/
+
+#include "mcc_generated_files/mcc.h"
+void MCC_USB_CDC_DemoTasks(void);
+/*
+                         Main application
+ */
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    while (1)
+    {
+       MCC_USB_CDC_DemoTasks();
+        // Add your application code
+       //Fetch next command
+       //execute 
+    }
+}
+
+//USB interruption
+//Read command 
+//save in a FIFO queue
+//out
+
+//Next angle interrupt (external interrupt)
+//read timer
+//clear timer
+//recalculate speed
+//out
+
+
+
+/**
+ End of File
+*/
